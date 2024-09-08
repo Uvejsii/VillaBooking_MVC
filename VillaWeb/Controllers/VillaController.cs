c3d3b092a6eb35bede03db91dd71a1d1dd8cc20d
Uvejsii <136826106+Uvejsii@users.noreply.github.com>
2024-09-08 22:54:18 +0200

    connected to db and rendered seeded villas

---
+++ VillaWeb/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VillaBooking.Infrastructure.Data;
+
+namespace VillaBooking.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
