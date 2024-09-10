e02fe2de231034407e277378bc1dfa7cdecb9902
Uvejsii <136826106+Uvejsii@users.noreply.github.com>
2024-09-10 20:14:01 +0200

    added db table and CRUD for Villa Number

---
+++ VillaWeb/Controllers/VillaNumberController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VillaBooking.Domain.Entities;
+using VillaBooking.Infrastructure.Data;
+
+namespace VillaBooking.Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var villas = _db.VillaNumbers.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VillaNumber obj) 
+        {
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumbers.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The Villa Number has been created successfully.";
+
+                RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Update(int villaNumber) 
+        {
+            VillaNumber? obj = _db.VillaNumbers.Find(villaNumber);
+
+            if (obj == null) 
+            {
+                return RedirectToAction("Error", "Home");            
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update (VillaNumber obj) 
+        {
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumbers.Update(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int villaNumber) 
+        {
+            VillaNumber? obj = _db.VillaNumbers.Find(villaNumber);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(VillaNumber obj)
+        {
+            VillaNumber? objFromDb = _db.VillaNumbers.FirstOrDefault(vn => vn.Villa_Number == obj.Villa_Number);
+
+            if (objFromDb is not null)
+            {
+                _db.VillaNumbers.Remove(objFromDb);
+                _db.SaveChanges();
+                TempData["success"] = "The Villa Number has been deleted successfully.";
+
+                RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
